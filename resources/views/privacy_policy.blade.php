943e651dd095e047ceb7e285a73d40a53cc5ec1e
Marco <marco.faltoni@outlook.it>
2020-07-01 16:20:36 +0200

    Privacy Policy fatta

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -1,7 +1,83 @@
 @extends('layouts.app')
 
-@section('title', 'Privacy Policy')
+@section('title', 'Privacy Policy - Boolean Careers')
 
 @section('content')
-    <h1>prova3</h1>
+    <div class="wrap-policy">
+        <h1>Privaci Policy</h1>
+        <p>Ai sensi dell’art. 13 del Regolamento UE 2016/679 (di seguito “GDPR”), Boolean S.R.L., con sede legale in Piazzale Giovanni dalle Band Nere 9, 20146 Milano rappresentata da Fabio Forghieri e Roberto Marazzini n qualità di Titolari del trattamento dei dati personali, La informa che i Suoi dati verranno trattati nel rispetto dei principi di correttezza, liceità e trasparenza e nel rispetto dei diritti e libertà fondamentali e della Sua dignità, oltreché delle norme sul segreto professionale e d’ufficio.</p>
+        <section>
+            <h4>Natura dei dati trattati</h4>
+            <p>I dati sono raccolti direttamente da Boolean S.R.L. da fonti pubbliche (se relativi allo Studente) e attraverso la compilazione volontaria del modulo di registrazione/iscrizione.</p>
+            <p>Mediante la registrazione l’Utente acconsente a fornire i propri dati per l’esecuzione dei servizi prestati e/o per l’acquisto dei prodotti venduti sul presente Sito. <br>
+            I dati personali raccolti sono: Nome, Cognome, Data di Nascita, Numero di Telefono, Indirizzo di Residenza o di esercitazione della professione, Username, Email, Password, Ragione sociale, Codice Fiscale, Partita IVA e Professione, l’indirizzo IP ed altri dati riferibili alle generalità dell’Utente o alle modalità tecniche di accesso dell’Utente al Sito.
+            Altri dati raccolti per il completamento del profilo di ogni utente (denominato “Studente”) riguardano la propria professionalità ed includono, a titolo esemplificativo: titolo di studio, età, anni di esperienza lavorativa e informazioni curriculari.
+            I dati di navigazione utili per l’utilizzo tecnico del Sito sono raccolti necessariamente e il loro conferimento non è facoltativo.
+            Il mancato trattamento di alcuni dati (es. i dati di contatto, o gli indirizzi, o dati sul comportamento o sulle preferenze dell’utente ecc.) potrebbe rendere impossibile la prestazione dei servizi da parte del Sito. Se l’Utente utilizza il Sito per pubblicare o comunque condividere dati personali di terzi, (Interessati diversi dall’Utente) ne assume le relative responsabilità. In tal senso, l’Utente garantisce di avere il diritto di comunicare o diffondere i dati personali dei terzi e di averli previamente informati del trattamento, sollevando il Sito da ogni responsabilità per il loro trattamento strumentale in seguito alla loro comunicazione o diffusione.</p>
+        </section>
+        <section>
+            <h4>Finalità del trattamento</h4>
+            <p>I Dati dell’Utente o dell’Interessato sono raccolti per consentire:</p>
+            <ul>
+                <li>l’erogazione del servizio;</li>
+                <li>la risposta a specifiche richieste informative dell’Utente;</li>
+                <li>l’aggiornamento dell’Utente sui nuovi prodotti o servizi erogati dal Sito o in relazione a particolari offerte commerciali;</li>
+                <li>l’informazione dell’Utente in occasione di manutenzione sul Sito o di eventuali disservizi;</li>
+                <li>l’eventuale profilazione commerciale dell’Utente;</li>
+                <li>la visualizzazione di banner pubblicitari in relazione agli interessi dell’Utente (behavioral targeting/advertising);</li>
+                <li>la difesa da parte del Titolare del Sito, in giudizio o nelle fasi propedeutiche alla sua eventuale instaurazione, da abusi nell’utilizzo dello stesso o dei servizi connessi da parte dell’Utente.</li>
+            </ul>
+        </section>
+        <section>
+            <h4>Base giuridica e consenso</h4>
+            <p>I Suoi dati personali potranno essere trattati dal Titolare in caso sussista una delle seguenti condizioni:</p>
+            <ul>
+                <li>sia stato prestato il consenso per una o più finalità specifiche ai sensi dell’art. 6 lett. a) del GDPR (ad esempio nel caso di invio di comunicazioni promozionali, profilazione commerciale, behavioral advertising, geolocalizzazione ecc.);</li>
+                <li>il trattamento è necessario all'esecuzione di un contratto con l’Utente e/o all'esecuzione di misure precontrattuali ai sensi dell’art. 6 lett. b) del GDPR;</li>
+                <li>il trattamento è necessario per adempiere un obbligo legale al quale è soggetto il Titolare ai sensi dell’art. 6 lett. c) del GDPR;</li>
+                <li>il trattamento è necessario per il perseguimento del legittimo interesse del Titolare o di terzi.</li>
+            </ul>
+            <p>È comunque sempre possibile richiedere al Titolare di chiarire la concreta base giuridica di ciascun trattamento ed in particolare di specificare se il trattamento sia basato sulla legge, previsto da un contratto o necessario per concludere un contratto.</p>
+        </section>
+        <section>
+            <h4>Modalità di trattamento dei dati</h4>
+            <p>I dati personali conferiti vengono trattati attraverso mezzi elettronici e/o cartacei, secondo i principi di correttezza, liceità, trasparenza e di tutela della riservatezza e dei diritti. Possono essere utilizzate, inoltre, altre modalità (audio, video, ecc.) ritenute utili caso per caso. I dati sono inoltre protetti mediante misure tecniche ed organizzative per assicurare idonei livelli di sicurezza e riservatezza ai sensi degli artt. 25 e 32 del GDPR. </p>
+        </section>
+        <section>
+            <h4>Diffusione, Comunicazione dei dati e destinatari</h4>
+            <p>I dati personali non sono oggetto di diffusione (intendendosi per tale, il dare conoscenza di dati personali a soggetti indeterminati, in qualunque forma, anche mediante la loro messa a disposizione o consultazione), fatta salva l’ipotesi in cui la diffusione sia richiesta, in conformità alla legge, da forze di polizia, dall’autorità giudiziaria, da organismi di informazione e di sicurezza o da altri soggetti pubblici per finalità di difesa o si sicurezza dello Stato o di prevenzione, accertamento o prevenzione di reati.  I dati personali potranno invece essere utilizzati, per le finalità precedentemente esposte o per finalità ulteriori, da soggetti terzi, di seguito indicati, che prestano servizi che consentono al Sito di gestire e monitorare elenchi d’indirizzi email utilizzati per comunicare con l’Utente tramite posta elettronica:</p>
+            <ul>
+                <li>Direct Email Marketing (DEM): questo Sito può utilizzare i Dati dell’Utente, se dallo stesso previamente autorizzato, per inviare proposte di natura commerciale in relazione a servizi e prodotti forniti da terze parti. In caso di promozione di prodotti o servizi del titolare analoghi a quelli venduti all’interessato non è necessario il consenso, ma l’interessato, può opporsi in ogni momento a nuovi invii. I dati personali raccolti sono: Email, Cookie, Indirizzo IP, Nome e Cognome.</li>
+                <li>Soggetti che forniscono servizi di pubblicità consentendo al Sito di utilizzare i Dati dell’Utente per finalità di comunicazione commerciale visualizzate sotto forma di banner e altre forme pubblicitarie sul Sito in relazione agli interessi dell’Utente;</li>
+                <li>Soggetti che forniscono servizi di statistica che tengono traccia del comportamento dell’Utente ed, in caso di profilazione, possono richiedere il consenso espresso dell’Utente (Google Analytics, Social Plugin, Google + …).</li>
+            </ul>
+        </section>
+        <section>
+            <h4>Link ad altri Siti Web</h4>
+            <p>Sulle pagine web di boolean.careers si possono trovare collegamenti ipertestuali (Hyperlink) con altri siti web, proposti per fornire un migliore servizio ai propri utenti. boolean.careers non è in alcun modo responsabile del contenuto dei siti web ai quali gli utenti dovessero eventualmente accedere tramite il proprio sito. L’esistenza di un Hyperlink verso un altro sito non sottintende quindi approvazione od accettazione di responsabilità da parte di boolean.careers circa il contenuto del nuovo sito cui si accede, anche in relazione alla politica adottata per il trattamento dei dati personali, nonché alla sua utilizzazione.</p>
+        </section>
+        <section>
+            <h4>Definizioni e riferimenti legali</h4>
+            <p>Cookie - Il cookie è una porzione di dati che viene memorizzata su un computer o un device mobile quando navighi in internet, affinché il computer o il device mobile possa essere riconosciuto in un momento successivo. I cookies da noi utilizzati non ti profilano, non ti identificano, ma si limitano a riconoscere esclusivamente il dispositivo che utilizzi. I cookies non danneggiano in alcun modo il tuo dispositivo, ma abilitano molteplici importanti caratteristiche del Sito internet. Ti permettono, ad esempio, di tornare alle pagine che avevi cercato; proteggono boolean.careers; ricordano le tue preferenze; personalizzano il contenuto del Sito in modo che sia più adatto a te. Utilizziamo i cookies, inoltre, per raccogliere informazioni sul modo in cui utilizzi il Sito. Utilizziamo queste informazioni esclusivamente per compilare rapporti e come ausilio per migliorare il Sito. La raccolta avviene in forma anonima e riguarda informazioni quali il numero di visitatori del Sito, da dove provengono i visitatori, le pagine visitate. Un cookie non ci dà in nessun modo accesso al tuo computer e non ci fornisce nessuna informazione su di te, oltre ai dati che tu decidi di condividere con noi. Sei libero di scegliere se accettare o meno i cookies. La maggior parte dei browser accetta in automatico i cookies. E’ possibile modificare le impostazioni del tuo browser allo scopo di rifiutare i cookies, se lo preferisci. A tal fine ti invitiamo a consultare le seguenti pagine:</p>
+            <ul>
+                <li>Chrome: <a href="https://support.google.com/chrome#topic=7438008">Link</a></li>
+                <li>Firefox: <a href="https://support.mozilla.org/en-US/">Link</a></li>
+                <li>internet Explorer: <a href="https://www.microsoft.com/en-us/windows">Link</a></li>
+                <li>Opera: <a href="https://help.opera.com/en/latest/">Link</a></li>
+                <li>Safari: <a href="https://support.apple.com/">Link</a></li>
+            </ul>
+            <p>Se decidi di non abilitare i cookies, la tua esperienza su boolean.careers potrebbe essere limitata.</p>
+        </section>
+        <section>
+            <h4>Trasferimento dei dati personali extra UE</h4>
+            <p>Per quanto concerne l’eventuale futuro trasferimento di dati verso Paesi terzi, il Titolare rende noto che l’eventuale trattamento avverrà nel rispetto della normativa, ovvero secondo una delle modalità consentite dalla legge vigente, quali ad esempio: </p>
+            <ul>
+                <li>Trasferimento verso Paesi che offrono garanzie di protezione adeguate;</li>
+                <li>Adozione di clausole contrattuali standard approvate dalla Commissione Europea; </li>
+                <li>Adozione di norme vincolanti autorizzate dall’Autorità Garante; </li>
+                <li>Selezione di soggetti aderenti a programmi internazionali per la libera circolazione dei dati (es. EU-USA) </li>
+            </ul>
+            <p>Nel caso in cui, per trattamenti specifici, i dati dovessero in futuro essere trasferiti ad organizzazioni internazionali o verso soggetti dislocati in Stati non appartenenti all’Unione Europea, il Titolare fornirà comunque una informazione specifica al riguardo. </p>
+        </section>
+    </div>
 @endsection
